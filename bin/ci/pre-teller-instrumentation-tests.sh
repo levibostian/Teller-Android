d7be7c803062b56d17a3dc7f4a982a8f099463d3
Levi Bostian <levi.bostian@gmail.com>
2019-02-18 10:47:04 -0600

    Add code coverage for CI tests coverage

--- bin/ci/pre-teller-instrumentation-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p teller/build/outputs/apk/debug/
-
-cp static/app-debug-apk-testing.apk teller/build/outputs/apk/debug/app-debug.apk
-
-# Bitrise hack to change modified date of the file to the current time. 
-touch teller/build/outputs/apk/debug/app-debug.apk
