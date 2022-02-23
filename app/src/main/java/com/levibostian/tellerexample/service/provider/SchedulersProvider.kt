ca2e7e2002d235e9accac2c978adeea2d1ba05d0
Levi Bostian <levi.bostian@gmail.com>
2022-02-23 07:25:30 -0600

    build: setup for maven central publishing (#85)

--- app/src/main/java/com/levibostian/tellerexample/service/provider/SchedulersProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.levibostian.tellerexample.service.provider
-
-import io.reactivex.Scheduler
-
-interface SchedulersProvider {
-    fun io(): Scheduler
-    fun mainThread(): Scheduler
-}
--- bin/githooks/pre-commit.rb
+++
@@ -1,6 +0,0 @@
-require 'trent'
-
-ci = Trent.new(color: :light_blue, local: true)
-
-ci.sh('./gradlew :teller-android:dokka')
-ci.sh('git add docs/')
